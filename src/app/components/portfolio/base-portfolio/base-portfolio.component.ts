c2c921510a3633980d2c3b58cf94f0c0024e66e5
maharjanprogress <progressmaharjan1@gmail.com>
2025-10-10 00:09:41 +0545

    will now be able to call api

--- src/app/components/portfolio/base-portfolio/base-portfolio.component.ts
+++ src/app/components/portfolio/base-portfolio/base-portfolio.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import {SnackbarService} from "../../../services/snackbar.service";
 import {ImageService} from "../../../services/image/image.service";
 import {Model3dService} from "../../../services/3d/model3d.service";
+import {Subscription} from "rxjs";
+import {ExperienceService} from "../../../services/experience/experience.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-base-portfolio',
@@ -11,11 +14,16 @@ import {Model3dService} from "../../../services/3d/model3d.service";
   styleUrl: './base-portfolio.component.scss'
 })
 export class BasePortfolioComponent {
-  constructor(private snackbarService: SnackbarService,private imageService:ImageService,private modelService: Model3dService) {
+
+  private subscriptions= new Subscription();
+  constructor(private snackbarService: SnackbarService,
+              private imageService:ImageService,
+              private modelService: Model3dService,
+              private experienceService: ExperienceService) {
   }
 
   onPress(){
-    this.snackbarService.show("this is just a test", "success");
+    this.snackbarService.show("this is just a test", "info");
   }
 
   onImage() {
@@ -25,5 +33,18 @@ export class BasePortfolioComponent {
 
   on3Dmodel() {
     this.modelService.open("https://test.ismart.devanasoft.com.np/ismart/bankApps/RadhaOliResidency2/b120b2a265e74d02b7c0517252b1c78f_opt.glb");
+  }
+
+  onLoadExp(){
+    this.subscriptions.add(
+      this.experienceService.getExperience().subscribe({
+        next:(response) => {
+          console.log(response);
+        },
+        error:(err:HttpErrorResponse)=>{
+          console.log(err);
+        }
+      })
+    )
   }
 }
