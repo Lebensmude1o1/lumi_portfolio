0255e73b2b1e0e71dc7b88bfc32942c527c8b599
maharjanprogress <progressmaharjan1@gmail.com>
2025-10-10 16:17:40 +0545

    correct UserId now in place

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://personalportfoliobackend-1.onrender.com',
-  userId: 1,
+  userId: 3,
   enableLogging: true,
   username: 'Sadiksha Shrestha'
 };
